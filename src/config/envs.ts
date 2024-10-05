392c77a515204760a369c31b9c0f2818c33bcf18
Bruno Galván <brunogalvangarcia@outlook.com>
2024-10-05 01:14:18 -0500

    chore: update configuration and handler

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,10 +3,6 @@ import { get } from "env-var";
 
 export const envs = {
   SWAPI_API_URL: get("SWAPI_API_URL").required().asString(),
-
   AWS_REGION: get("AWS_REGION").required().asString(),
-  AWS_ACCESS_KEY_ID: get("AWS_ACCESS_KEY_ID").required().asString(),
-  AWS_SECRET_ACCESS_KEY: get("AWS_SECRET_ACCESS_KEY").required().asString(),
-
   DYNAMODB_TABLE: get("DYNAMODB_TABLE").required().asString(),
 };
